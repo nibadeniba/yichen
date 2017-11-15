33858385ce3774ce6af5dac50f85d4085fa22a46
Zhaokaihui <254665468@qq.com>
2017-11-15 10:07:32 +0800

    联系我们

--- app/controllers/ContactController.php
+++ app/controllers/ContactController.php
@@ -9,7 +9,22 @@ class ContactController extends BaseController
 	{	
 
 		$nav_active = 'contact';
-
-		return View::make('web/contact', compact('nav_active'));
+		$imgs = CMS::where("cate", 'contact_map')->first();
+		$title = CMS::where("cate", 'contact_title')->first();
+		$text = CMS::where("cate", 'contact_text')->first();
+		$share = CMS::where("cate", 'contact_share')->first();
+		$companyinfo = CMS::where('cate','contact_companyinfo')->first();
+		$wechatimg = CMS::where('cate','contact_wechatimg')->first();
+        
+		return View::make('web/contact', compact('nav_active','title','imgs','text','share','companyinfo','wechatimg'));
+		
+		
+// 		$imgs = CMS::where("cate", 'index_img')->get();
+// 		$cards = CMS::where("cate", "index_card")->get();
+// 		$text = CMS::where("cate", "index_text")->first();
+		
+// 		$nav_active = 'index';
+		
+// 		return View::make('web/index', compact('nav_active', "imgs", "cards", "text"));
 	}
 }
